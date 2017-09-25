084ebf6bb49f1da276e7ea6eedb2c796b49401bf
Steven Gago <sgago@cvtc.edu>
2017-09-25 15:27:50 -0500

    Default.css
    Added a default.css file.  Updated the website header, logo image, icons, etc.

---
+++ index/example for t dog.java
@@ -0,0 +1,15 @@
+private float costVariable = 2.0f;
+
+public void setCostFunction(float costParameter) {
+  // Puts the costParameter into costVariable
+  // We stick what's on the right into what's on the left
+  costVariable = costParameter;
+}
+
+// Common errors and what they mean:
+// Cannot be resolved means, the variable doesn't
+// exist (type) or it can't be found
+
+// Common warnings and what they mean:
+// Blah blah blah, is not used.
+// The variable/object isn't being used
